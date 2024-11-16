fbf571c405a7a4fe5159e445436aa54933767548
javirrof <javirofe96@gmail.com>
2024-11-16 10:02:30 +0100

    use default port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 async function bootstrap() {
   const DEFAULT_PORT = 5000
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
-  await app.listen(process.env.PORT ?? 5000)
+  await app.listen(process.env.PORT ?? DEFAULT_PORT)
 }
 bootstrap()
