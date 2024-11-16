02894e7a657a23ff8e4fc68dfdf95b37e2f538d3
javirrof <javirofe96@gmail.com>
2024-11-16 08:43:30 +0100

    Add Nest basic structure

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
+
+async function bootstrap() {
+  const DEFAULT_PORT = 5000
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  await app.listen(process.env.PORT ?? 5000)
+}
+bootstrap()
