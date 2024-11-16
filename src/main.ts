f5c21ee4b37cd264e9e530663174ab0f74996c1c
javirrof <javirofe96@gmail.com>
2024-11-16 10:15:46 +0100

    add CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { NestExpressApplication } from '@nestjs/platform-express'
 async function bootstrap() {
   const DEFAULT_PORT = 5000
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.enableCors({
+    origin: process.env.UI_URL,
+    methods: 'GET,PUT,POST,DELETE',
+    preflightContinue: false,
+  })
   await app.listen(process.env.PORT ?? DEFAULT_PORT)
 }
 bootstrap()
