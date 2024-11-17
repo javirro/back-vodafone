15bd44bb2da5b426a98e60886aecee867535e0d3
javirrof <javirofe96@gmail.com>
2024-11-17 16:33:58 +0100

    remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { DevicesController } from './devices/devices.controller'
 import { DevicesService } from './devices/devices.service'
 
 @Module({
   imports: [],
-  controllers: [AppController, DevicesController],
-  providers: [AppService, DevicesService],
+  controllers: [DevicesController],
+  providers: [DevicesService],
 })
 export class AppModule {}
