5134685818a0034332b05b2c00ed42242d4b2b40
javirrof <javirofe96@gmail.com>
2024-11-17 12:17:43 +0100

    update configuration with database_url

--- src/db/connectionDB.ts
+++ src/db/connectionDB.ts
@@ -2,13 +2,25 @@ import { Pool } from 'pg'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const dbClient = new Pool({
+const DATABASE_URL = process.env.DATABASE_URL
+let dbClient = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   port: Number(process.env.DB_PORT),
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 })
+if (DATABASE_URL) {
+  const params = new URL(DATABASE_URL)
+  const config = {
+    host: params.hostname,
+    user: params.username,
+    port: Number(params.port),
+    password: params.password,
+    database: params.pathname.split('/')[1],
+  }
+  dbClient = new Pool(config)
+}
 
 export default dbClient
 
